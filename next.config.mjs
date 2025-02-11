fe185084aa124e42225d02dce6cc1c939909bd3c
bilal27263 <bilalaiyadi04@gmail.com>
2025-02-11 11:24:55 +0000

    Removed API key from tracked files

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-// filepath: /workspaces/traveleasy1/next.config.mjs
-export default {
-    reactStrictMode: true,
-    env: {
-      NEXT_PUBLIC_SUPABASE_URL: "https://znczwhlreoqbvtzywfmb.supabase.co",
-      NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpuY3p3aGxyZW9xYnZ0enl3Zm1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxMTgyMTEsImV4cCI6MjA1NDY5NDIxMX0.iPj-cO-gOXJToaWs6P8X7fdhIUJsB7hByzElySMUBgg",
-    },
-  };
