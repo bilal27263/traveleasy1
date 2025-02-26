4905d8e24001b48c6b1404304f64b3338586a111
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2025-02-26 15:57:08 +0100

    new commit

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@
 // import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
+
 export async function middleware(req: NextRequest) {
   // const res = NextResponse.next()
   //const supabase = createMiddlewareClient({ req, res })
